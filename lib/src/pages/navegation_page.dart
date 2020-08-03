4570c16a5c697a68d70ad401c924d10b80bdbe75
Gpspidey19 <gianpaulcustodio1198@gmail.com>
2020-08-02 23:22:17 -0500

    Aumentador de notificaciones con presionar un click

--- lib/src/pages/navegation_page.dart
+++ lib/src/pages/navegation_page.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:app1/src/twitter.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -44,17 +45,27 @@ class BotonNavigation extends StatelessWidget {
                     //     Icon(Icons.brightness_1, size: 8, color: Colors.pink),
                     top: 0.0,
                     right: -1.0,
-                    child: Container(
-                      child: Text(
-                        '$numero',
-                        style: TextStyle(color: Colors.white, fontSize: 12),
-                      ),
-                      alignment: Alignment.center,
-                      width: 13,
-                      height: 13,
-                      decoration: BoxDecoration(
-                        color: Colors.red,
-                        shape: BoxShape.circle,
+                    child: BounceInDown(
+                      // from: 10,
+                      animate: (numero > 0) ? true : false,
+                      child: Bounce(
+                        from: 10,
+                        controller: (controller) =>
+                            Provider.of<_NotificationModel>(context)
+                                .bounceController = controller,
+                        child: Container(
+                          child: Text(
+                            '$numero',
+                            style: TextStyle(color: Colors.white, fontSize: 12),
+                          ),
+                          alignment: Alignment.center,
+                          width: 13,
+                          height: 13,
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            shape: BoxShape.circle,
+                          ),
+                        ),
                       ),
                     )),
               ],
@@ -73,11 +84,17 @@ class BotonFlotante extends StatelessWidget {
       child: FaIcon(FontAwesomeIcons.play),
       backgroundColor: Colors.red,
       onPressed: () {
-        int numero =
-            Provider.of<_NotificationModel>(context, listen: false).numero;
+        final notiModel =
+            Provider.of<_NotificationModel>(context, listen: false);
+
+        int numero = notiModel.numero;
         numero++;
 
-        Provider.of<_NotificationModel>(context, listen: false).numero = numero;
+        notiModel.numero = numero;
+
+        if (numero >= 2) {
+          final controller = notiModel.bounceController.forward(from: 0.0);
+        }
       },
     );
   }
@@ -85,10 +102,18 @@ class BotonFlotante extends StatelessWidget {
 
 class _NotificationModel extends ChangeNotifier {
   int _numero = 0;
+  AnimationController _bounceController;
+
   int get numero => _numero;
 
   set numero(int valor) {
     this._numero = valor;
+    notifyListeners();
+  }
+
+  AnimationController get bounceController => this._bounceController;
+  set bounceController(AnimationController controller) {
+    this._bounceController = controller;
     notifyListeners();
   }
 }
